b92ba9a94708352276245b203f7d1e3a32a6027e
Erez Porat <erez@localhost.localdomain>
2024-07-27 19:38:47 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,19 +24,31 @@ public class Main {
       String requestLine = reader.readLine();
       String[] parts = requestLine.split(" ");
       String method = parts[0];
-      String path = parts[1];
+      String path;
 
-      if(path.equals("/")){
-        clientSocket.getOutputStream().write(
-        "HTTP/1.1 200 OK\r\n\r\n".getBytes()
-      );
-      }
+      String[] paths =parts[1].split("/");
+     if (paths.length > 1){
+
+       path = paths[1];
+      } 
       else{
+         path = "";
+      }
+
+      if(path.equals("echo")){
+        String response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + paths[2].length() + "\r\n\r\n" + paths[2];
+        clientSocket.getOutputStream().write(
+        response.getBytes()
+      );
+      } else if(path.equals("")){
+            clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+        
+      } else{
         clientSocket.getOutputStream().write(
           "HTTP/1.1 404 Not Found\r\n\r\n".getBytes()
         );
+      
       }
-
 
        System.out.println("accepted new connection");
      } catch (IOException e) {
